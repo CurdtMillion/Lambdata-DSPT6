38fc28e90165b6684b38af8c59c4aed4a056a849
CurdtMillion <curtcecil@gmail.com>
2020-07-11 22:56:58 -0500

    Updated README

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ A collection of data science utility functions.
 
 ## Installation
 
-# pip install -i https://test.pypi.org/simple/ U3M1-CurdtMillion==2.5
+pip install -i https://test.pypi.org/simple/ U3M1-CurdtMillion==2.5
 
 ## Usage
 
-# 4 simple functions to test module creation
+4 simple functions to test module creation: enlarge, decimate, checknan & addrow
+from my_lambdata.my_mod import (module_name_here)
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name="U3M1_CurdtMillion", 
-    version="2.5",
+    version="2.5.1",
     author="Curtis Cecil",
     author_email="curtcecil@gmail.com",
-    description="A small example package, v2.5 all funcs can be imported!",
+    description="A small example package, v2.5.1 all funcs can be imported! README updtd",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CurdtMillion/Lambdata-DSPT6",
